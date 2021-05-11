a138d63ab4640b7bdb12aa45f8bdd7ca2d35b641
tsunga59 <tatsumochi@Tatsu-MacBookPro.local>
2021-05-11 11:02:38 +0900

    add select memo method

--- resources/views/memo.blade.php
+++ resources/views/memo.blade.php
@@ -20,8 +20,7 @@
                 <p>メモがありません。</p>
             @endif
             @foreach($memos as $memo)
-            {{-- <a href="./action/select.php?id=<?php echo $memo['id']; ?>" class="<?php echo $edit_id == $memo['id'] ? 'active' : ''; ?>"> --}}
-                <a href="" class="">
+                <a href="{{ route('memo.select', ['id' => $memo->id]) }}" class="@if($selected_memo) {{ $selected_memo->id == $memo->id ? 'active' : '' }} @endif">
                     <p class="title">{{ $memo->title }}</p>
                     <span class="date">{{ date('Y/m/d H:i', strtotime($memo->updated_at)) }}</span>
                     <span class="description">{{ mb_substr($memo->content, 0, 25).'…' }}</span>
@@ -30,9 +29,9 @@
         </div>
     </div>
     <div class="memo_right">
-        {{-- <?php if(isset($_SESSION['selected_memo'])): ?> --}}
+        @if($selected_memo)
         <form action="" method="post">
-            <input type="hidden" name="edit_id" value="">
+            <input type="hidden" name="edit_id" value="{{ $selected_memo->id }}">
             <div class="top_area">
                 <div class="btn_area">
                     <button type="submit" formaction=""><i class="fas fa-pen"></i></button>
@@ -40,13 +39,13 @@
                 </div>
             </div>
             <div class="content_area">
-                <input type="text" name="edit_title" placeholder="タイトル…" value="" class="title">
-                <textarea name="edit_content" placeholder="詳細テキスト…" class="content"></textarea>
+                <input type="text" name="edit_title" placeholder="タイトル…" value="{{ $selected_memo->title }}" class="title">
+                <textarea name="edit_content" placeholder="詳細テキスト…" class="content">{{ $selected_memo->content }}</textarea>
             </div>
         </form>
-        {{-- <?php else: ?>
-        <p>メモを新規作成または選択してください。</p>
-        <?php endif; ?> --}}
+        @else
+            <p>メモを新規作成または選択してください。</p>
+        @endif
     </div>
 </section>
 
